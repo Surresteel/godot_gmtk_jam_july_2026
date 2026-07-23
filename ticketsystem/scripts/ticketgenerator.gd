02938f0eafa58c0fd048a3f3bba020dbbac590a7
Kieza3131 <kieranlester@protonmail.com>
2026-07-24 05:29:50 +1000

    reset ticket not working yet

--- ticketsystem/scripts/ticketgenerator.gd
+++ ticketsystem/scripts/ticketgenerator.gd
@@ -1,20 +1,22 @@
 extends Node3D
 
+@onready var label: Label3D = $Label3D
 @export var ingredient_amount: int
 @export var ingredient_list: Array[ingredient]
 var ingredients: Array[ingredient]
+var ingredient_list_ready: Array[ingredient]
 
 func _ready() -> void:
 	
-	dish()
+	for i in ingredient_list:
+		ingredient_list_ready.append(i)
 	
 func _process(_delta: float) -> void:
 	pass
 
 func add_ingredient():
 	
-	var current_ingredient : ingredient = ingredient_list.get(randi_range(0,
-	 ingredient_list.size() - 1))
+	var current_ingredient : ingredient = ingredient_list.pick_random()
 	
 	if not ingredients.has(current_ingredient):
 		ingredients.append(current_ingredient)
@@ -22,16 +24,43 @@ func add_ingredient():
 		
 func dish():
 	
+	ingredient_list = ingredient_list_ready.duplicate()
+	print(ingredient_list.size())
+	
 	if ingredient_amount <= ingredient_list.size():
 		for i in range(0, ingredient_amount):
 			add_ingredient()
+			
+	ticket()
+
+func ticket():
 	
-		if not ingredients.is_empty():
-			for i in ingredients:
-				print(i.name)
-				if not i.cook.is_empty():
-					print(i.COOK.find_key(i.cook.pick_random()))
-				if not i.doneness.is_empty():
-					print(i.DONENESS.find_key(i.doneness.pick_random()))
-				if not i.prep.is_empty():
-					print(i.PREP.find_key(i.prep.pick_random()))
+	if not ingredients.is_empty():
+		
+		for i in ingredients:
+			
+			var cook_value = ""
+			if not i.cook.is_empty():
+				cook_value = i.COOK.find_key(i.cook.pick_random())
+			
+			var doneness_value = ""
+			if not i.doneness.is_empty():
+				doneness_value =i.DONENESS.find_key(i.doneness.pick_random())
+			
+			var prep_value = ""
+			if not i.prep.is_empty():	
+				prep_value = i.PREP.find_key(i.prep.pick_random())
+			
+				label.text = cook_value + " " \
+				+ i.name + " " + doneness_value \
+				+ " " + prep_value	
+				
+func _unhandled_input(event: InputEvent) -> void:
+	
+	if event is not InputEventMouseButton:
+		return
+	event = event as InputEventMouseButton
+
+	if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		
+		dish()
