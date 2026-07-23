3b47ed3ad5935d82b964270aa056da2c13a1382e
YeePomfPomf <131655763+yeepomfpomf@users.noreply.github.com>
2026-07-23 14:54:51 +1000

    something idk im falling asleep

--- Cooking/Heat Area/heat_area.gd
+++ Cooking/Heat Area/heat_area.gd
@@ -7,18 +7,16 @@ class_name HeatArea
 
 @export var heat_level: float = 1.0
 
-var current_ingridient
-
-signal increase_cook_level(amount: int)
+signal increase_cook_level(amount: int) 										##Signal Emitted when the current ingridient has been cooked for its appointed time
 
 
 func start_cooking() -> void:
-	#cook_timer.start(current_ingridient.time_to_cook)
+	#var cook_time = current_ingridient.get_cook_time(heat_level)
+	#cook_timer.start(cook_time)
 	pass
 
 func stop_cooking() -> void:
 	cook_timer.stop()
-	current_ingridient = null
 
 func _on_timer_timeout() -> void:
 	increase_cook_level.emit(1)
